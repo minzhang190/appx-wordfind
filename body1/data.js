70f8be16371830edc138e320c9492769aebb9ea2
minzhang190 <minzhang190@gmail.com>
2020-04-13 10:45:48 +0000

    +body1/2

---
+++ body1/data.js
@@ -0,0 +1,66 @@
+var title = "Shēntǐ";
+
+var words = {
+    "shenti": {
+        "image": "assets/5e943b2b25c505681d0f1dee.png",
+        "sound": "assets/5e943b2b25c505681d0f1dee.mp3"
+    },
+    "toufa": {
+        "image": "assets/5e8dac932c02d5b61d430631.png",
+        "sound": "assets/5e8dac932c02d5b61d430631.mp3"
+    },
+    "bozi": {
+        "image": "assets/5e8dac932c02d5b61d43063c.png",
+        "sound": "assets/5e8dac932c02d5b61d43063c.mp3"
+    },
+    "tou": {
+        "image": "assets/5e8dac932c02d5b61d43062e.png",
+        "sound": "assets/5e8dac932c02d5b61d43062e.mp3"
+    },
+    "jianbang": {
+        "image": "assets/5e8dac932c02d5b61d43062f.png",
+        "sound": "assets/5e8dac932c02d5b61d43062f.mp3"
+    },
+    "xiongpu": {
+        "image": "assets/5e943b2d25c505681d0f1def.png",
+        "sound": "assets/5e943b2d25c505681d0f1def.mp3"
+    },
+    "shou": {
+        "image": "assets/5e8dac932c02d5b61d43062d.png",
+        "sound": "assets/5e8dac932c02d5b61d43062d.mp3"
+    },
+    "shouzhi": {
+        "image": "assets/5e8dac932c02d5b61d430630.png",
+        "sound": "assets/5e8dac932c02d5b61d430630.mp3"
+    },
+    "jiao": {
+        "image": "assets/5e943b2e25c505681d0f1df0.png",
+        "sound": "assets/5e943b2e25c505681d0f1df0.mp3"
+    },
+    "jiaozhi": {
+        "image": "assets/5e8dac932c02d5b61d43063d.png",
+        "sound": "assets/5e8dac932c02d5b61d43063d.mp3"
+    },
+    "tui": {
+        "image": "assets/5e8dac932c02d5b61d43063b.png",
+        "sound": "assets/5e8dac932c02d5b61d43063b.mp3"
+    },
+    "xigai": {
+        "image": "assets/5e8dac932c02d5b61d430638.png",
+        "sound": "assets/5e8dac932c02d5b61d430638.mp3"
+    },
+    "gebo": {
+        "image": "assets/5e8dac932c02d5b61d43062c.png",
+        "sound": "assets/5e8dac932c02d5b61d43062c.mp3"
+    },
+    "gebozhou": {
+        "image": "assets/5e8dac932c02d5b61d43063f.png",
+        "sound": "assets/5e8dac932c02d5b61d43063f.mp3"
+    },
+    "duzi": {
+        "image": "assets/5e8dac932c02d5b61d430639.png",
+        "sound": "assets/5e8dac932c02d5b61d430639.mp3"
+    }
+};
+
+options.orientations = ['horizontal', 'vertical', 'diagonal', 'diagonalBack'];
---
+++ body2/data.js
@@ -0,0 +1,64 @@
+var title = "Shēntǐ";
+
+var words = {
+    "yachi": {
+        "image": "assets/5e8dac932c02d5b61d43063a.png",
+        "sound": "assets/5e8dac932c02d5b61d43063a.mp3"
+    },
+    "zuichun": {
+        "image": "assets/5e943b3125c505681d0f1df1.png",
+        "sound": "assets/5e943b3125c505681d0f1df1.mp3"
+    },
+    "zui": {
+        "image": "assets/5e8dac932c02d5b61d430636.png",
+        "sound": "assets/5e8dac932c02d5b61d430636.mp3"
+    },
+    "xiaba": {
+        "image": "assets/5e943b3225c505681d0f1df2.png",
+        "sound": "assets/5e943b3225c505681d0f1df2.mp3"
+    },
+    "qiane": {
+        "image": "assets/5e943b3225c505681d0f1df3.png",
+        "sound": "assets/5e943b3225c505681d0f1df3.mp3"
+    },
+    "yanjing": {
+        "image": "assets/5e8dac932c02d5b61d430634.png",
+        "sound": "assets/5e8dac932c02d5b61d430634.mp3"
+    },
+    "erduo": {
+        "image": "assets/5e8dac932c02d5b61d430633.png",
+        "sound": "assets/5e8dac932c02d5b61d430633.mp3"
+    },
+    "bizi": {
+        "image": "assets/5e8dac932c02d5b61d430635.png",
+        "sound": "assets/5e8dac932c02d5b61d430635.mp3"
+    },
+    "lian": {
+        "image": "assets/5e8dac932c02d5b61d430632.png",
+        "sound": "assets/5e8dac932c02d5b61d430632.mp3"
+    },
+    "jiaohuai": {
+        "image": "assets/5e943b3425c505681d0f1df4.png",
+        "sound": "assets/5e943b3425c505681d0f1df4.mp3"
+    },
+    "houlong": {
+        "image": "assets/5e943b3425c505681d0f1df5.png",
+        "sound": "assets/5e943b3425c505681d0f1df5.mp3"
+    },
+    "meimao": {
+        "image": "assets/5e8dac932c02d5b61d430637.png",
+        "sound": "assets/5e8dac932c02d5b61d430637.mp3"
+    },
+    "houbei": {
+        "image": "assets/5e8dac932c02d5b61d43063e.png",
+        "sound": "assets/5e8dac932c02d5b61d43063e.mp3"
+    },
+    "shetou": {
+        "image": "assets/5e943b3625c505681d0f1df6.png",
+        "sound": "assets/5e943b3625c505681d0f1df6.mp3"
+    },
+    "gutou": {
+        "image": "assets/5e943b3625c505681d0f1df7.png",
+        "sound": "assets/5e943b3625c505681d0f1df7.mp3"
+    }
+};
