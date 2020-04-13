9e9311601b63e94e9fa248d2260b33902a972f4b
minzhang190 <minzhang190@gmail.com>
2020-04-13 07:18:40 +0000

    +fruits1/2ez

---
+++ fruits1ez/data.js
@@ -0,0 +1,46 @@
+var title = "Shuǐguǒ (1)";
+
+var words = {
+    "pingguo": {
+        "image": "assets/5e8bf93af6082885ff046aa6.png",
+        "sound": "assets/5e8bf93af6082885ff046aa6.mp3"
+    },
+    "li": {
+        "image": "assets/5e8bf93bf6082885ff046aa7.png",
+        "sound": "assets/5e8bf93bf6082885ff046aa7.mp3"
+    },
+    "shuiguo": {
+        "image": "assets/5e8bf93cf6082885ff046aa8.png",
+        "sound": "assets/5e8bf93cf6082885ff046aa8.mp3"
+    },
+    "xiangjiao": {
+        "image": "assets/5e8bf93cf6082885ff046aa9.png",
+        "sound": "assets/5e8bf93cf6082885ff046aa9.mp3"
+    },
+    "putao": {
+        "image": "assets/5e8bf93df6082885ff046aaa.png",
+        "sound": "assets/5e8bf93df6082885ff046aaa.mp3"
+    },
+    "xigua": {
+        "image": "assets/5e8bf93ef6082885ff046aab.png",
+        "sound": "assets/5e8bf93ef6082885ff046aab.mp3"
+    },
+    "boluo": {
+        "image": "assets/5e8bf93ef6082885ff046aac.png",
+        "sound": "assets/5e8bf93ef6082885ff046aac.mp3"
+    },
+    "taozi": {
+        "image": "assets/5e8bf93ff6082885ff046aad.png",
+        "sound": "assets/5e8bf93ff6082885ff046aad.mp3"
+    },
+    "lizi": {
+        "image": "assets/5e8bf940f6082885ff046aae.png",
+        "sound": "assets/5e8bf940f6082885ff046aae.mp3"
+    },
+    "chengzi": {
+        "image": "assets/5e8bf941f6082885ff046aaf.png",
+        "sound": "assets/5e8bf941f6082885ff046aaf.mp3"
+    }
+};
+
+options.orientations = ['horizontal', 'vertical'];
---
+++ fruits2ez/data.js
@@ -0,0 +1,46 @@
+var title = "Shuǐguǒ (2)";
+
+var words = {
+    "shiliu": {
+        "image": "assets/5e8bf942f6082885ff046ab0.png",
+        "sound": "assets/5e8bf942f6082885ff046ab0.mp3"
+    },
+    "caomei": {
+        "image": "assets/5e8bf942f6082885ff046ab1.png",
+        "sound": "assets/5e8bf942f6082885ff046ab1.mp3"
+    },
+    "mangguo": {
+        "image": "assets/5e8bf943f6082885ff046ab2.png",
+        "sound": "assets/5e8bf943f6082885ff046ab2.mp3"
+    },
+    "ningmeng": {
+        "image": "assets/5e8bf944f6082885ff046ab3.png",
+        "sound": "assets/5e8bf944f6082885ff046ab3.mp3"
+    },
+    "yingtao": {
+        "image": "assets/5e8bf944f6082885ff046ab4.png",
+        "sound": "assets/5e8bf944f6082885ff046ab4.mp3"
+    },
+    "yezi": {
+        "image": "assets/5e8bf945f6082885ff046ab5.png",
+        "sound": "assets/5e8bf945f6082885ff046ab5.mp3"
+    },
+    "niuyouguo": {
+        "image": "assets/5e8bf946f6082885ff046ab6.png",
+        "sound": "assets/5e8bf946f6082885ff046ab6.mp3"
+    },
+    "heimei": {
+        "image": "assets/5e8bf946f6082885ff046ab7.png",
+        "sound": "assets/5e8bf946f6082885ff046ab7.mp3"
+    },
+    "lanmei": {
+        "image": "assets/5e8bf947f6082885ff046ab8.png",
+        "sound": "assets/5e8bf947f6082885ff046ab8.mp3"
+    },
+    "mihoutao": {
+        "image": "assets/5e8bf948f6082885ff046ab9.png",
+        "sound": "assets/5e8bf948f6082885ff046ab9.mp3"
+    }
+};
+
+options.orientations = ['horizontal', 'vertical'];
